b819c51f31d30ff719ca8e8c9cc50452007fa2ab
RTC12 <rtc12@rtc12de-imac.local>
2017-03-27 18:29:32 +0800

    Initial Commit

---
+++ new (2017-03-27)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  new (2017-03-27)
+//
+//  Created by RTC12 on 2017/3/27.
+//  Copyright © 2017年 RTC12. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
